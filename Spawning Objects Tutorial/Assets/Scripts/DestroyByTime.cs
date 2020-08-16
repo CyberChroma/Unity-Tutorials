f2e7db7a280e78ecf7564b65072a84ad08ddbd93
Brandon Howe <cyberchroma@hotmail.com>
2020-08-15 21:10:55 -0400

    Spawning Objects Tutorial

---
+++ Spawning Objects Tutorial/Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByTime : MonoBehaviour
+{
+    public float lifetime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+}
---
+++ Spawning Objects Tutorial/Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public float range;
+    public float delay;
+    public GameObject[] objectsToSpawn;
+
+    private Transform spawnParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnParent = GameObject.Find("Spawned Objects").transform;
+        StartCoroutine(WaitToSpawn());
+    }
+
+    IEnumerator WaitToSpawn ()
+    {
+        yield return new WaitForSeconds(delay);
+        Spawn();
+    }
+
+    void Spawn ()
+    {
+        Instantiate(objectsToSpawn[Random.Range(0, objectsToSpawn.Length)], new Vector3 (transform.position.x + Random.Range(-range, range), transform.position.y, transform.position.z), Random.rotation, spawnParent);
+        StartCoroutine(WaitToSpawn());
+    }
+}
