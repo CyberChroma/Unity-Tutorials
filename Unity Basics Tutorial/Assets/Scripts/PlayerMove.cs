8f6299dc64087fc16d6c08a1f4740cbde375db22
Brandon Howe <cyberchroma@hotmail.com>
2020-08-15 20:08:59 -0400

    Unity Basics Tutorial

---
+++ Unity Basics Tutorial/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.right * -speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.forward * -speed * Time.deltaTime;
+        }
+    }
+}
