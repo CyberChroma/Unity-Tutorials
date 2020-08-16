2dc9c1d5199d41fbf0b46cc8c8ec8b4383b56620
Brandon Howe <cyberchroma@hotmail.com>
2020-08-15 21:17:16 -0400

    Character Jumping

---
+++ Character Jumping Tutorial/Assets/Scripts/PlayerGroundCheck.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGroundCheck : MonoBehaviour
+{
+    [HideInInspector] public bool isGrounded;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            isGrounded = false;
+        }
+    }
+}
---
+++ Character Jumping Tutorial/Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    public float jumpForce;
+    public float gravityMultiplier;
+    public float fallMultiplier;
+    public float lowJumpMultiplier;
+
+    private Rigidbody rb;
+    private PlayerGroundCheck playerGroundCheck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerGroundCheck = GetComponentInChildren<PlayerGroundCheck>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+    }
+
+    void FixedUpdate()
+    {
+        Fall();
+    }
+
+    void Jump ()
+    {
+        if (playerGroundCheck.isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce * 10, ForceMode.Impulse);
+        }
+    }
+
+    void Fall ()
+    {
+        if (!playerGroundCheck.isGrounded)
+        {
+            if (rb.velocity.y >= 0 && !Input.GetKey(KeyCode.Space))
+            {
+                rb.AddForce(Vector3.down * lowJumpMultiplier * 10);
+            } else if (rb.velocity.y < 0)
+            {
+                rb.AddForce(Vector3.down * fallMultiplier * 10);
+            }
+
+            rb.AddForce(Vector3.down * gravityMultiplier * 10);
+        }
+    }
+}
