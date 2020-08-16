01c8aa37ba8eac3b8e34cfa06e46e242d46fe48b
Brandon Howe <cyberchroma@hotmail.com>
2020-08-15 21:15:54 -0400

    1st and 3rd Person Tutorial

---
+++ 1st and 3rd Person Tutorial/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public float rotSpeed;
+    public bool lockCursor;
+
+    private float rotX;
+    private float rotY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotX = transform.rotation.eulerAngles.x;
+        rotY = transform.rotation.eulerAngles.y;
+        if (lockCursor)
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotX += -Input.GetAxis("Mouse Y") * rotSpeed * Time.deltaTime;
+        rotY += Input.GetAxis("Mouse X") * rotSpeed * Time.deltaTime;
+        rotX = Mathf.Clamp(rotX, -85, 85);
+        transform.rotation = Quaternion.Euler(new Vector3(rotX, rotY, 0));
+    }
+}
---
+++ 1st and 3rd Person Tutorial/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed;
+    public float drag;
+    public Transform mCam;
+
+    private float movVert;
+    private float movHorz;
+    private Vector3 moveDir;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector3(rb.velocity.x * drag, rb.velocity.y, rb.velocity.z * drag);
+        if (Input.GetKey(KeyCode.W))
+        {
+            movVert = 1;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            movVert = -1;
+        }
+        else
+        {
+            movVert = 0;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            movHorz = 1;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            movHorz = -1;
+        }
+        else
+        {
+            movHorz = 0;
+        }
+        moveDir = mCam.right * movHorz + mCam.forward * movVert;
+        moveDir = new Vector3(moveDir.x, 0, moveDir.z).normalized;
+        rb.AddForce(moveDir * speed, ForceMode.Impulse);
+    }
+}
